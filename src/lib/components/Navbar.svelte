1a7dc2196533be065e1c13688c35fcd51242a36d
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-08-11 09:37:05 +0200

    first commit

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<nav class="flex">
+    <div class="flex flex-1">
+        <a class="cursor-pointer" href="/">
+            <img src="/logo.webp" alt="Logo" class="w-48 h-48" />
+        </a>
+        <div class="my-auto">
+            <span
+                class="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-amber-400 to-pink-600"
+                >JITx</span
+            >
+
+            <div class="my-auto italic">
+                AI Projects and Software Engineering
+            </div>
+            <div class="text-xs text-gray-400">by Arthur A. Galstyan</div>
+        </div>
+    </div>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Navbar from "$lib/components/Navbar.svelte";
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <title>JITx</title>
+</svelte:head>
+<main class="w-[80%] mx-auto">
+    <Navbar />
+    <slot />
+</main>
