120fc3a5b1bc7726505bdf46aa6abae66b037737
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-08-11 09:41:36 +0200

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import Navbar from "$lib/components/Navbar.svelte";
     import "../app.css";
+
+    import { dev } from "$app/environment";
+    import { inject } from "@vercel/analytics";
+
+    inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
