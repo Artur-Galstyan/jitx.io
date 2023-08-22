6e655ed67f18bb9d6c2e426e4368f6a47d414947
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-08-22 09:57:28 +0200

    removed mdsvex because it doesnt work

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import { mdsvex } from "mdsvex";
 
-import mdsvexConfig from "./mdsvex.config.js";
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    extensions: [".svelte", ...mdsvexConfig.extensions],
+    extensions: [".svelte"],
 
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+    preprocess: [vitePreprocess()],
 
     kit: {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
