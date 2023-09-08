7be768031b19b6bad8d2865645f4f45c794b2842
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-09-08 16:53:17 +0200

    make draft hidden

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,25 +9,27 @@
 
 <div class="font-extrabold text-lg">Posts</div>
 {#each $page.data.posts as post}
-    <div class="flex space-x-2">
-        <div class="my-auto">
-            {new Date(post.createdAt).toLocaleDateString("en-CA", {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit"
-            })}
+    {#if !post.draft}
+        <div class="flex space-x-2">
+            <div class="my-auto">
+                {new Date(post.createdAt).toLocaleDateString("en-CA", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit"
+                })}
+            </div>
+            <div class="my-auto flex-1">
+                {post.title}
+            </div>
+            <button
+                class="btn btn-xs btn-info btn-outline my-auto"
+                on:click={() => {
+                    goto(`posts/${post.slug}`);
+                }}
+            >
+                Read
+            </button>
         </div>
-        <div class="my-auto flex-1">
-            {post.title}
-        </div>
-        <button
-            class="btn btn-xs btn-info btn-outline my-auto"
-            on:click={() => {
-                goto(`posts/${post.slug}`);
-            }}
-        >
-            Read
-        </button>
-    </div>
-    <div class="divider" />
+        <div class="divider" />
+    {/if}
 {/each}
