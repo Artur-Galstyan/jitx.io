40d715a58a5cc0dc0207832ef64a59132becf3c3
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-09-24 22:35:55 +0200

    draft and published

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,18 +9,23 @@
 
 <div class="font-extrabold text-lg">Posts</div>
 {#each $page.data.posts as post}
-    <div class="flex space-x-2">
-        {#if post.draft}
-            <div class="text-warning">Draft</div>
-        {/if}
-        <div class="my-auto">
-            {new Date(post.createdAt).toLocaleDateString("en-CA", {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit"
-            })}
+    <div class="flex flex-col md:flex-row md:space-x-2">
+        <div class="md:w-[30%] flex space-x-4">
+            <div class="my-auto text-gray-400">
+                {new Date(post.createdAt).toLocaleDateString("en-CA", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit"
+                })}
+            </div>
+
+            {#if post.status === "DRAFT"}
+                <div class="text-warning">Draft</div>
+            {:else if post.status === "PLANNED"}
+                <div class="text-info">Planned</div>
+            {/if}
         </div>
-        <div class="my-auto flex-1">
+        <div class="my-auto flex-1 font-bold">
             {post.title}
         </div>
         <button
