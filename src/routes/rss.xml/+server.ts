38239edba0efb29bea6f9bee2d04cc67d344272b
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-11-07 00:29:01 +0100

    added rss

---
+++ src/routes/rss.xml/+server.ts
@@ -0,0 +1,46 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import { json } from "@sveltejs/kit";
+import { prisma } from "$lib/server/prisma.server";
+
+export const GET = (async ({ request, fetch }) => {
+  const posts = await prisma.post.findMany({
+    where: {
+      NOT: {
+        status: "HIDDEN",
+      },
+    },
+  });
+
+  // xml headers
+  const headers = { "Content-Type": "application/rss+xml" };
+
+  const xml = `
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0"> 
+    <channel>
+  <title>JITx</title>
+  <link>https://jitx.io</link>
+  <description>AI and SE by Artur A. Galstyan</description>
+
+    ${posts
+      .map((post) => {
+        `
+        <item>
+        <title>${post.title}</title>
+        <link>https://jitx.io/posts/${post.slug}</link>
+        <description>${post.shortDescription}</description> 
+        <pubDate>${post.updatedAt}</pubDate>
+        <author>Artur A. Galstyan</author>
+        <guid>https://jitx.io/posts/${post.slug}</guid>
+        </item>
+       `;
+      })
+      .join("")}
+   
+  </channel>
+</rss> 
+    
+    `;
+
+  return json(0);
+}) satisfies RequestHandler;
