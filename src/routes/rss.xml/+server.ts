feb5b273ca6416216581ef037793f6f148f4c94b
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-11-07 09:27:31 +0100

    rss fix

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -15,12 +15,13 @@ export const GET = (async ({ request, fetch }) => {
 
   const xml = `
     <?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0"> 
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom> 
     <channel>
   <title>JITx</title>
   <link>https://jitx.io</link>
   <description>AI and SE by Artur A. Galstyan</description>
-
+  <channel> 
+    <atom:link href="/rss.xml" rel="self" type="application/rss+xml"/>
     ${posts
       .map((post) => {
         `
