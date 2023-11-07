10461a36bef564b2ac4c3e9d4729285306602df8
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-11-07 09:46:30 +0100

    oops

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -15,7 +15,7 @@ export const GET = (async ({ request, fetch }) => {
 
   const xml = `
     <?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom> 
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"> 
     <channel>
     <title>JITx.io</title>
     <link>https://jitx.io</link>
