42fcedb054a73d37e55fe597c102277db33c3c72
Artur A. Galstyan <galstyan.artu@gmail.com>
2023-11-19 23:26:56 +0100

    reinforce

--- src/routes/posts/reinforce-policy-gradient/+page.svelte
+++ src/routes/posts/reinforce-policy-gradient/+page.svelte
@@ -135,5 +135,108 @@
         is the current iteration. Simply put, take the current parameters, add the gradient on top, and voilá, you got
         the next set of parameters.
     </p>
+    <p>
+        Alright, let's get started with the policy gradient. These steps will be a bit more mathematically heavy, so
+        buckle up! I'll try to explain each step along the way.
+    </p>
+    <div class="divider">The Gradient</div>
+    <p>
+        <Katex math={`\\nabla_\\theta J(\\pi_\\theta) = \\nabla_\\theta \\mathbb{E}_{\\tau \\sim \\pi_\\theta} \\left [ R(\\tau) \\right] `}
+               displayMode={true}/>
+    </p>
+    <div class="divider">Expand Expectation</div>
+    <p>
+        <Katex math={`=\\nabla_\\theta \\int_T P(\\tau | \\theta)R(\\tau)`} displayMode={true}/>
+    </p>
+    <div class="divider">Move Gradient into the Integral</div>
+    <p>
+        <Katex math={`= \\int_T \\nabla_\\theta \\big(P(\\tau | \\theta)\\big) R(\\tau)`} displayMode={true}/>
+    </p>
+    <p>
+        Everything up until now should have been very easy. Now we get to one of the more tricky parts. We need to
+        perform the so-called <i>log-derivative trick</i>. We do that because computing the gradient of the probability
+        directly is
+        very complex and also because logs introduce a lot of numerical stability, especially as numbers get smaller.
+    </p>
+    <p>
+        The log-derivative trick is quite simple: the derivative of
+        <Katex math={`\\log x`}/>
+        is
+        <Katex math={`\\frac{1}{x}`}/>
+        . So we start with
+        <Katex math={`\\log P(\\tau |\\theta)`}/>
+        and want to compute the gradient w.r.t.
+        <Katex math={`\\theta`}/>
+        .
+    </p>
+    <div class="divider">Apply the Gradient & the Chain Rule</div>
+    <Katex math={`\\nabla_\\theta \\log P(\\tau | \\theta)`} displayMode={true}/>
+    <div class="divider">Differentiating the Outer Function (Logarithm)</div>
+    <Katex math={`\\frac{d}{dx}\\log x = \\frac{1}{x}`} displayMode={true}/>
+    <Katex math={` = \\frac{1}{P(\\tau | \\theta)}`} displayMode={true}/>
+    <div class="divider">Differentiating the Inner Function (Probability Function)</div>
+    <Katex math={`\\nabla_\\theta P(\\tau | \\theta)`} displayMode={true}/>
+    <div class="divider">Multiplying the Derivatives as per Chain Rule</div>
+    <Katex math={`\\frac{1}{P(\\tau | \\theta)} \\nabla_\\theta P(\\tau | \\theta)`} displayMode={true}/>
+    <div class="divider">Chain Rule Result</div>
+    <Katex math={`\\nabla_\\theta \\log P(\\tau |\\theta) = \\frac{1}{P(\\tau|\\theta)}  \\nabla_\\theta P(\\tau | \\theta) `}
+           displayMode={true}/>
+    <div class="divider">Rearranging to Isolate
+        <p>
+            <Katex math={`\\nabla_\\theta P(\\tau | \\theta)`}/>
+        </p>
+    </div>
+    <p class="text-center">Multiply the probabilities on both sides</p>
+    <Katex math={` P(\\tau | \\theta) \\nabla_\\theta \\log P(\\tau |\\theta) = P(\\tau | \\theta) \\frac{1}{P(\\tau|\\theta)} \\nabla_\\theta P(\\tau | \\theta) `}
+           displayMode={true}/>
+    <div class="divider">Cancel Stuff out</div>
+    <Katex math={`\\nabla_\\theta P(\\tau | \\theta) = P(\\tau | \\theta) \\nabla_\\theta \\log P(\\tau |\\theta) `}
+           displayMode={true}/>
+    <p>Alright, now you know about the log-derivative trick. Let's go back to the gradient of the objective function. We
+        had just brought in the gradient into the integral.</p>
+    <div class="divider">(Reminder) Move Gradient into the Integral</div>
+    <p>
+        <Katex math={`= \\int_T \\nabla_\\theta \\big(P(\\tau | \\theta)\\big) R(\\tau)`} displayMode={true}/>
+    </p>
+    <div class="divider">Put in the Result from the Log-Derivative Trick</div>
+    <p>
+        <Katex math={`= \\int_T P(\\tau | \\theta) \\nabla_\\theta \\Big( \\log P(\\tau |\\theta) \\Big) R(\\tau)`}
+               displayMode={true}/>
+    </p>
+    <div class="divider">Back To Expectation</div>
+    <Katex math={`= \\mathbb{E}_{\\tau \\sim \\pi} \\Big [ \\nabla_\\theta \\log P(\\tau | \\theta) R(\\tau) \\Big ]`}
+           displayMode={true}/>
+    <p>Now it's time to have a closer look at this term
+        <Katex math={`\\nabla_\\theta \\log P(\\tau | \\theta)`}/>
+        . First, applying the log, we get
 
+        <Katex math={`\\log P(\\tau|\\theta) = \\log p_0(s_0) + \\sum_{t=0}^{T} \\left( \\log P(s_{t+1}|s_t, a_t) + \\log \\pi_{\\theta}(a_t|s_t) \\right)
+`} displayMode={true}/>
+        Simple stuff so far: just apply the log to every term in the equation. Next, we apply the gradient operation to
+        each term (because the derivative of a sum is the sum of the derivatives). We get:
+        <Katex math={`\\nabla_\\theta \\log P(\\tau|\\theta) = \\nabla_\\theta\\log p_0(s_0) + \\sum_{t=0}^{T} \\left( \\nabla_\\theta(\\log P(s_{t+1}|s_t, a_t)) + \\nabla_\\theta( \\log \\pi_{\\theta}(a_t|s_t)) \\right)
+`} displayMode={true}/>
+        In this equation, each term which has nothing to do with
+        <Katex math={`\\theta`}/>
+        becomes
+        <Katex math={`0`}/>
+        . Removing those, we get:
+        <Katex math={`\\nabla_\\theta \\log P(\\tau|\\theta) = \\cancel{\\nabla_\\theta\\log p_0(s_0)} + \\sum_{t=0}^{T} \\left( \\cancel{\\nabla_\\theta(\\log P(s_{t+1}|s_t, a_t))} + \\nabla_\\theta( \\log \\pi_{\\theta}(a_t|s_t)) \\right)
+`} displayMode={true}/>
+        <Katex math={`\\nabla_\\theta \\log P(\\tau|\\theta) =\\sum_{t=0}^{T}  \\nabla_\\theta\\log \\pi_{\\theta}(a_t|s_t)
+`} displayMode={true}/>
+    </p>
+    <div class="divider">(Reminder) Back To Expectation</div>
+    <Katex math={`= \\mathbb{E}_{\\tau \\sim \\pi} \\Big [ \\nabla_\\theta \\log P(\\tau | \\theta) R(\\tau) \\Big ]`}
+           displayMode={true}/>
+    <div class="divider">Replacing
+        <p>
+            <Katex math={`\\nabla_\\theta \\log P(\\tau|\\theta)`}/>
+        </p>
+    </div>
+    <Katex math={`\\nabla_{\\theta} J(\\pi_{\\theta}) = \\mathbb{E}_{\\tau \\sim \\pi_{\\theta}} \\left[ \\sum_{t=0}^{T} \\nabla_{\\theta} \\log \\pi_{\\theta}(a_t|s_t) R(\\tau) \\right]
+`} displayMode={true}/>
+    <p>
+        Uff, that was a lot of math. Let's take a quick break before we continue! Time for a coffee!
+    </p>
 </section>
