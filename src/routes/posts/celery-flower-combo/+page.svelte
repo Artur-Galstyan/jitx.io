7b773bc87ee9087ab0bcd7993f5188337de5a80d
Artur A. Galstyan <galstyan.artu@gmail.com>
2024-01-13 21:49:09 +0100

    almost there

--- src/routes/posts/celery-flower-combo/+page.svelte
+++ src/routes/posts/celery-flower-combo/+page.svelte
@@ -147,7 +147,613 @@ services:
   />
   <p>
     We can now run <code>docker compose up</code> and see if everything works.
-    If you go to <code>localhost</code> in your browser, you should see the default
-    Nginx page.
   </p>
+</section>
+<section>
+  <h3>RabbitMQ</h3>
+  <p>
+    Now, we will setup RabbitMQ. We will use the official RabbitMQ docker image
+    for this. Let's add this part to the <code>docker-compose.yml</code> file.
+  </p>
+  <CodeBox
+    code={`
+version: "3.8"
+services:
+  rabbitmq:
+    image: rabbitmq:3-management
+    hostname: rabbit_hostname
+    container_name: rabbitmq
+    environment:
+      RABBITMQ_DEFAULT_USER: user
+      RABBITMQ_DEFAULT_PASS: password
+    ports:
+      - "4369:4369"
+      - "5671:5671"
+      - "5672:5672"
+      - "15671:15671"
+      - "15691-15692:15691-15692"
+      - "25672:25672"
+      - "15672:15672"
+  nginx:
+    build:
+      context: .
+      dockerfile: Dockerfile.nginx
+    volumes:
+      - /etc/letsencrypt:/etc/letsencrypt
+    ports:
+      - "80:80"
+      - "443:443"
+    depends_on:
+      - rabbitmq`}
+    filename="docker-compose.yml"
+    language="yaml"
+  />
+  <p>
+    You will notice that I also forwarded some ports. I'm actually not sure if
+    we even need all of them at this point. We will do an ablation study later.
+    For now, I've just added all of them. We also want the Nginx container to
+    start up last, so we add a <code>depends_on</code> field.
+  </p>
+  <p>
+    Let's go ahead and add this to the <code>nginx.conf</code> file.
+  </p>
+  <CodeBox
+    code={`
+
+user nginx;
+worker_processes 2;
+
+error_log /var/log/nginx/error.log;
+pid /var/run/nginx.pid;
+
+events {
+  worker_connections 1024;
+  use epoll;
+}
+http {
+  upstream rabbitmq_management {
+    server rabbitmq:15672;
+  }
+
+  server {
+    listen 80;
+    # server_name your_domain.com;
+    
+    # comment this if you want to use https
+    location / {
+      proxy_pass http://rabbitmq_management;
+      proxy_set_header Host $http_host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+      proxy_read_timeout 300;
+      proxy_connect_timeout 300;
+      proxy_redirect off;
+    }
+
+    # uncomment this if you want to use https 
+    # return 301 https://$host$request_uri;
+  }
+  
+  # uncomment this if you want to use https
+  # server {
+  #   listen 443 ssl;
+  #   server_name your_domain.com;
+  #
+  #   ssl_certificate /etc/letsencrypt/live/your_domain.com/fullchain.pem; # Adjust to your certificate's path
+  #   ssl_certificate_key /etc/letsencrypt/live/your_domain.com/privkey.pem; # Adjust to your key's path
+  #   include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+  #
+  #   location / {
+  #     proxy_pass http://rabbitmq_management;
+  #     proxy_set_header Host $http_host;
+  #     proxy_set_header X-Real-IP $remote_addr;
+  #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  #     proxy_set_header X-Forwarded-Proto $scheme;
+  #     proxy_read_timeout 300;
+  #     proxy_connect_timeout 300;
+  #     proxy_redirect off;
+  #   }
+  # }
+}
+`}
+    filename="nginx.conf"
+    language="nginx"
+  />
+  <p>
+    Running <code>docker compose up</code> should now start up both Nginx and
+    RabbitMQ. If you navigate to
+    <code>localhost:15672</code>, you should see the RabbitMQ management
+    interface. You can login with the credentials we set in the
+    <code>docker-compose.yml</code> file.
+    <Figure path="rabbitmqsite.webp" caption="RabbitMQ Management" />
+    That's all we need for now.
+  </p>
+</section>
+<section>
+  <h3>Postgres</h3>
+  <p>
+    Next up is Postgres. We will use the official Postgres docker image for
+    this. Let's add this part to the <code>docker-compose.yml</code> file.
+  </p>
+  <CodeBox
+    code={`
+version: "3.8"
+services:
+  rabbitmq:
+    image: rabbitmq:3-management
+    hostname: rabbit_hostname
+    container_name: rabbitmq
+    environment:
+      RABBITMQ_DEFAULT_USER: user
+      RABBITMQ_DEFAULT_PASS: password
+    ports:
+      - "4369:4369"
+      - "5671:5671"
+      - "5672:5672"
+      - "15671:15671"
+      - "15691-15692:15691-15692"
+      - "25672:25672"
+      - "15672:15672"
+  postgres:
+    image: postgres:latest
+    environment:
+      POSTGRES_DB: main
+      POSTGRES_USER: user
+      POSTGRES_PASSWORD: password
+    ports:
+      - "5432:5432"
+  nginx:
+    build:
+      context: .
+      dockerfile: Dockerfile.nginx
+    volumes:
+      - /etc/letsencrypt:/etc/letsencrypt
+    ports:
+      - "80:80"
+      - "443:443"
+    depends_on:
+      - rabbitmq 
+      - postgres 
+`}
+    filename="docker-compose.yml"
+    language="yaml"
+  />
+</section>
+<section>
+  <h3>Flower</h3>
+  <p>
+    The next step is setting up Flower. We will use the official Flower docker
+    image for that. Let's add that to our <code>docker-compose.yml</code> file.
+  </p>
+  <CodeBox
+    code={`
+
+version: "3.8"
+services:
+  rabbitmq:
+    image: rabbitmq:3-management
+    hostname: rabbit_hostname
+    container_name: rabbitmq
+    environment:
+      RABBITMQ_DEFAULT_USER: user
+      RABBITMQ_DEFAULT_PASS: password
+    ports:
+      - "4369:4369"
+      - "5671:5671"
+      - "5672:5672"
+      - "15671:15671"
+      - "15691-15692:15691-15692"
+      - "25672:25672"
+      - "15672:15672"
+  postgres:
+    image: postgres:latest
+    environment:
+      POSTGRES_DB: main
+      POSTGRES_USER: user
+      POSTGRES_PASSWORD: password
+    ports:
+      - "5432:5432"
+  flower:
+    image: mher/flower
+    environment:
+      - FLOWER_PERSISTENT=True
+      - FLOWER_DB=/etc/db/flower.db
+    depends_on:
+      - rabbitmq
+    command: celery --broker=amqp://user:password@localhost:5672// flower --basic-auth=some_user:some_password
+    ports:
+      - "5555:5555"
+    volumes:
+      - ./flower/storage:/etc/db/
+  nginx:
+    build:
+      context: .
+      dockerfile: Dockerfile.nginx
+    volumes:
+      - /etc/letsencrypt:/etc/letsencrypt
+    ports:
+      - "80:80"
+      - "443:443"
+    depends_on:
+      - rabbitmq 
+`}
+    filename="docker-compose.yml"
+    language="yaml"
+  />
+  <p>
+    We will also need to add some stuff to our <code>nginx.conf</code> file.
+  </p>
+  <CodeBox
+    code={`
+# ... the rest of the file
+  upstream flower {
+    server flower:5555;
+  }
+
+  server {
+    listen 80;
+    # comment this if you want to use https
+    # server_name your_domain.com;
+    
+    # comment this if you want to use https
+    location / {
+      limit_except GET POST OPTIONS {
+        deny all;
+      }
+      proxy_pass_header Server;
+      proxy_set_header Host $http_host;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_pass http://flower;
+
+      if ($request_method = 'OPTIONS') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+        add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+        add_header 'Access-Control-Max-Age' 86400;
+        add_header 'Content-Type' 'text/plain charset=UTF-8';
+        add_header 'Content-Length' 0;
+        return 204;
+      }
+      add_header 'Access-Control-Allow-Origin' '*';
+      add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+      add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+    }
+    # uncomment this if you want to use https
+    # return 301 https://$host$request_uri;
+  }
+  # uncomment this if you want to use https
+  # server {
+  #   listen 443 ssl;
+  #   server_name your_domain.com;
+  #   client_max_body_size 40m;
+  #   ssl_certificate /etc/letsencrypt/live/your_domain.com/fullchain.pem;
+  #   ssl_certificate_key /etc/letsencrypt/live/your_domain.com/privkey.pem;
+  #   include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+  #
+  #   location / {
+  #     limit_except GET POST OPTIONS {
+  #       deny all;
+  #     }
+  #     proxy_pass_header Server;
+  #     proxy_set_header Host $http_host;
+  #     proxy_http_version 1.1;
+  #     proxy_set_header Upgrade $http_upgrade;
+  #     proxy_set_header Connection "upgrade";
+  #     proxy_pass http://flower;
+  #
+  #     if ($request_method = 'OPTIONS') {
+  #       add_header 'Access-Control-Allow-Origin' '*';
+  #       add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+  #       add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+  #       add_header 'Access-Control-Max-Age' 86400;
+  #       add_header 'Content-Type' 'text/plain charset=UTF-8';
+  #       add_header 'Content-Length' 0;
+  #       return 204;
+  #     }
+  #     add_header 'Access-Control-Allow-Origin' '*';
+  #     add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+  #     add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+  #   }
+  # }
+
+  # ... the rest of the file
+`}
+    filename="nginx.conf"
+    language="nginx"
+  />
+  <p>
+    If you navigate to <code>localhost:5555</code>, you should see the Flower
+    dashboard. You will have to login with the credentials we set in the
+    <code>docker-compose.yml</code>
+    file. Furthermore, if you try to perform a GET request to, e.g.,
+    <code>localhost:5555/api/workers</code>, you should get a 401 error. This is
+    good for us, because we don't want unauthorized users to make requests to
+    our Flower API (which is the gateway to our Celery workers). By adding to
+    the GET request a authorization header with the credentials we set in the
+    <code>docker-compose.yml</code> file, we can make the request!
+  </p>
+</section>
+<section>
+  <h3>Monitoring: Grafana and Prometheus</h3>
+  <p>
+    Next up we will setup the monitoring tools for our Celery workers, which are
+    Grafana and Prometheus. Let's use the official Grafana and Prometheus docker
+    images for this and add that to our <code>docker-compose.yml</code> file.
+  </p>
+  <CodeBox
+    code={`
+
+version: "3.8"
+services:
+  rabbitmq:
+    image: rabbitmq:3-management
+    hostname: rabbit_hostname
+    container_name: rabbitmq
+    environment:
+      RABBITMQ_DEFAULT_USER: user
+      RABBITMQ_DEFAULT_PASS: password
+    ports:
+      - "4369:4369"
+      - "5671:5671"
+      - "5672:5672"
+      - "15671:15671"
+      - "15691-15692:15691-15692"
+      - "25672:25672"
+      - "15672:15672"
+  postgres:
+    image: postgres:latest
+    environment:
+      POSTGRES_DB: main
+      POSTGRES_USER: user
+      POSTGRES_PASSWORD: password
+    ports:
+      - "5432:5432"
+  flower:
+    image: mher/flower
+    container_name: flower
+    environment:
+      - FLOWER_PERSISTENT=True
+      - FLOWER_DB=/etc/db/flower.db
+    depends_on:
+      - rabbitmq
+    command: celery --broker=amqp://user:password@localhost:5672// flower --basic-auth=some_user:some_password
+    ports:
+      - "5555:5555"
+    volumes:
+      - ./flower/storage:/etc/db/
+  prometheus:
+        container_name: prometheus
+        volumes:
+            - ./prometheus.yml:/etc/prometheus/prometheus.yml
+        ports:
+            - 9090:9090
+        image: prom/prometheus
+  grafana:
+      container_name: grafana
+      volumes:
+          - ./grafana_storage:/var/lib/grafana
+      ports:
+          - 3000:3000
+      image: grafana/grafana
+  nginx:
+    build:
+      context: .
+      dockerfile: Dockerfile.nginx
+    volumes:
+      - /etc/letsencrypt:/etc/letsencrypt
+    ports:
+      - "80:80"
+      - "443:443"
+    depends_on:
+      - rabbitmq
+      - flower
+      - prometheus 
+      - grafana
+`}
+    filename="docker-compose.yml"
+    language="yaml"
+  />
+  <p>
+    We will also need to add some stuff to our <code>nginx.conf</code> file.
+  </p>
+  <CodeBox
+    code={`
+  # ... the rest of the file
+  upstream prometheus {
+    server prometheus:9090;
+  }
+
+  server {
+    listen 80;
+    # comment this if you want to use https
+    location / {
+      limit_except GET POST OPTIONS {
+        deny all;
+      }
+      proxy_pass_header Server;
+      proxy_set_header Host $http_host;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_pass http://prometheus;
+
+      if ($request_method = 'OPTIONS') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+        add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+        add_header 'Access-Control-Max-Age' 86400;
+        add_header 'Content-Type' 'text/plain charset=UTF-8';
+        add_header 'Content-Length' 0;
+        return 204;
+      }
+      add_header 'Access-Control-Allow-Origin' '*';
+      add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+      add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+    }
+    # server_name your_domain.com;
+    
+    # uncomment this if you want to use https
+    # return 301 https://$host$request_uri;
+  }
+
+  # server {
+  #   listen 443 ssl;
+  #   server_name your_domain.com;
+  #   client_max_body_size 40m;
+  #   ssl_certificate /etc/letsencrypt/live/your_domain.com/fullchain.pem;
+  #   ssl_certificate_key /etc/letsencrypt/live/your_domain.com/privkey.pem;
+  #   include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+  #
+  #   location / {
+  #     limit_except GET POST OPTIONS {
+  #       deny all;
+  #     }
+  #     proxy_pass_header Server;
+  #     proxy_set_header Host $http_host;
+  #     proxy_http_version 1.1;
+  #     proxy_set_header Upgrade $http_upgrade;
+  #     proxy_set_header Connection "upgrade";
+  #     proxy_pass http://prometheus;
+  #
+  #     if ($request_method = 'OPTIONS') {
+  #       add_header 'Access-Control-Allow-Origin' '*';
+  #       add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+  #       add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+  #       add_header 'Access-Control-Max-Age' 86400;
+  #       add_header 'Content-Type' 'text/plain charset=UTF-8';
+  #       add_header 'Content-Length' 0;
+  #       return 204;
+  #     }
+  #     add_header 'Access-Control-Allow-Origin' '*';
+  #     add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+  #     add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+  #   }
+  # }
+
+  upstream grafana {
+    server grafana:3000;
+  }
+
+  server {
+    listen 80;
+    
+    # comment this if you want to use https
+    location / {
+      proxy_pass_header Server;
+      proxy_set_header Host $http_host;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_pass http://grafana;
+
+      if ($request_method = 'OPTIONS') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+        add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+        add_header 'Access-Control-Max-Age' 86400;
+        add_header 'Content-Type' 'text/plain charset=UTF-8';
+        add_header 'Content-Length' 0;
+        return 204;
+      }
+      add_header 'Access-Control-Allow-Origin' '*';
+      add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+      add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+    }
+    # uncomment this if you want to use https
+    # server_name your_domain.com;
+    # return 301 https://$host$request_uri;
+  }
+
+  # server {
+  #   listen 443 ssl;
+  #   server_name your_domain.com;
+  #   client_max_body_size 40m;
+  #   ssl_certificate /etc/letsencrypt/live/your_domain.com/fullchain.pem;
+  #   ssl_certificate_key /etc/letsencrypt/live/your_domain.com/privkey.pem;
+  #   include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+  #
+  #   location / {
+  #     proxy_pass_header Server;
+  #     proxy_set_header Host $http_host;
+  #     proxy_http_version 1.1;
+  #     proxy_set_header Upgrade $http_upgrade;
+  #     proxy_set_header Connection "upgrade";
+  #     proxy_pass http://grafana;
+  #
+  #     if ($request_method = 'OPTIONS') {
+  #       add_header 'Access-Control-Allow-Origin' '*';
+  #       add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+  #       add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+  #       add_header 'Access-Control-Max-Age' 86400;
+  #       add_header 'Content-Type' 'text/plain charset=UTF-8';
+  #       add_header 'Content-Length' 0;
+  #       return 204;
+  #     }
+  #     add_header 'Access-Control-Allow-Origin' '*';
+  #     add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+  #     add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+  #   }
+  # }
+  # ... the rest of the file
+`}
+    filename="nginx.conf"
+    language="nginx"
+  />
+  <p>
+    We will also need to add a <code>prometheus.yml</code> file, which will be the
+    configuration for Prometheus.
+  </p>
+  <CodeBox
+    code={`
+global:
+  scrape_interval:     15s
+  evaluation_interval: 15s
+
+scrape_configs:
+  - job_name: prometheus
+    static_configs:
+      - targets: ['prometheus:9090']
+  - job_name: flower
+    static_configs:
+      - targets: ['flower:5555']
+`}
+    filename="prometheus.yml"
+    language="yaml"
+  />
+  <p>
+    One thing to note is that since both Grafana and Prometheus are running
+    based on the same <code>docker-compose.yml</code> file, we can use the
+    container names as the hostnames for the <code>prometheus.yml</code> file.
+    This is why we have
+    <code>prometheus:9090</code> and <code>flower:5555</code> as the targets. We
+    also create a couple of folders for Grafana and Prometheus to store their data
+    in.
+  </p>
+  <HintBox
+    title="Hint"
+    content="It would also make since to protect these monitoring sites using Nginx. That's left as an exercise for the reader."
+  />
+  <p>
+    Once you run <code>docker compose up</code>, you should be able to navigate
+    to
+    <code>localhost:3000</code> and see the Grafana dashboard. I would then
+    highly suggest you read
+    <a
+      href="https://flower.readthedocs.io/en/latest/prometheus-integration.html#"
+      >the tutorial provided from Flower</a
+    >
+    to setup the Prometheus and Grafana dashboards. When you setup the monitoring,
+    the connection URL in the Grafana dashboard should be
+    <code>http://prometheus:9090</code>
+    rather than <code>http://localhost:9090</code>, since we are running
+    everything using the same network and the container name is
+    <code>prometheus</code>. Finally, it should look like this:
+  </p>
+  <Figure path="grafana.webp" caption="Grafana Dashboard" />
+</section>
+<section>
+  <h3>Backend API</h3>
 </section>
