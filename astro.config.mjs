f37e2bfe674c62be31c02de55b09723ba312bd43
Artur Galstyan <mail@arturgalstyan.dev>
2024-03-30 16:00:35 +0100

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import remarkToc from "remark-toc";
-import remarkMath from "remark-math";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkToc from "remark-toc";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({ applyBaseStyles: false })],
   markdown: {
-    shikiConfig: {
-      theme: "catppuccin-macchiato",
-      wrap: false,
-    },
+    shikiConfig: { theme: "catppuccin-macchiato", wrap: false },
     remarkPlugins: [remarkMath, remarkToc],
     rehypePlugins: [rehypeKatex],
   },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
